28d34610518966ba11eee75c05e7006dd9d8d867
sid.salunke <sid.salunke@myob.com>
2019-03-28 19:09:26 +1100

    Move static markup for work examples into React components

--- js/main.js
+++ js/main.js
@@ -1,5 +1,31 @@
-import 'react-dom';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ExampleWork from './example-work';
 
-console.log('Loaded react-dom');
-
-console.log('Webpack works');
+const myWork = [
+  {
+    'title': "Senior Quality Analyst",
+    'img': {
+      'desc': "Work example when working at MYOB Group as a Senior Quality Analyst",
+      'src': "images/example1.png",
+      'comment': "",
+    }
+  },
+  {
+    'title': "BT Officer",
+    'img': {
+      'desc': "Work example when working at Suncorp Group as a Business Technology Officer",
+      'src': "images/example2.png",
+      'comment': "",
+    }
+  },
+  {
+    'title': "Programmer",
+    'img': {
+      'desc': "Work example when working at Cognizant Technology Solutions as a Programmer",
+      'src': "images/example3.png",
+      'comment': "",
+    }
+  }
+];
+ReactDOM.render(<ExampleWork work={myWork}/>, document.getElementById('example-work'));
