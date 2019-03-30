3b4c3eced807bf5808a2268f2eef52c49fdf32b7
sid.salunke <sid.salunke@myob.com>
2019-03-30 18:59:29 +1100

    Update website content and styling

--- __tests__/test-example-work-modal.js
+++ __tests__/test-example-work-modal.js
@@ -14,7 +14,13 @@ const myExample = {
     'desc': "Work example when working at MYOB Group as a Senior Quality Analyst",
     'src': "images/example1.png",
     'comment': "",
-  }
+  },
+  'jobResponsibilities': [
+    {'role': 'Writing and maintaining program code using Node.js, WebdriverIO, Cucumber and Chai to expand test coverage for end to end acceptance test suite'},
+    {'role': 'Building an automated smoke test suite from scratch using Node.js, WebdriverIO and Cucumber for all the products that the team handles'},
+    {'role': 'Setting up a build pipeline for the smoke test suite using an automated build management tool called Buildkite'},
+    {'role': 'Mentoring and upskilling fellow QAs by conducting training sessions and brown bags'}
+  ],
 };
 
 describe("ExampleWorkModal component", () => {
@@ -23,6 +29,7 @@ describe("ExampleWorkModal component", () => {
   let openComponent = shallow(<ExampleWorkModal example={myExample} 
     open={true}/>)
   let anchors = component.find("a");
+  let jobResponsibilities = component.find("li");
   
   it("should contain a single 'a' element", () => {
     expect(anchors.length).toEqual(1);
@@ -33,7 +40,11 @@ describe("ExampleWorkModal component", () => {
   });
 
   it("should have the modal class set correctly", () => {
-    expect(component.find(".background--skyBlue").hasClass("modal--closed")).toBe(true);
-    expect(openComponent.find(".background--skyBlue").hasClass("modal--open")).toBe(true);
+    expect(component.find(".background--white").hasClass("modal--closed")).toBe(true);
+    expect(openComponent.find(".background--white").hasClass("modal--open")).toBe(true);
   })
+
+  it("should render the correct number of job responsibilties", () => {
+    expect(jobResponsibilities.length).toEqual(4);
+  });
 })
